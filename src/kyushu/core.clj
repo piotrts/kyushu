c70e6e6397c072693c0817825b517a0dfcc3a0e7
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-05-26 22:46:45 +0200

    use even more data-oriented approach

--- src/kyushu/core.clj
+++ src/kyushu/core.clj
@@ -1,39 +1,31 @@
 (ns kyushu.core
   (:require [clojure.java.io :as io]))
 
-(defn file [path]
-  {:type :file
-   :path path
-   :data (try
-           (-> path io/file slurp)
-           (catch Exception e
-             {}))})
+(defn- assoc-data [source data]
+  (assoc-in source [1 :data] data))
 
-(defn resource [path]
-  {:type :resource
-   :path path
-   :data (try
-           (-> path io/resource slurp)
-           (catch Exception e
-             {}))})
+(defmulti load-handler (comp :type val))
 
-(defn memory [data]
-  {:type :memory
-   :data data})
+(defmethod load-handler :kyushu/file [source]
+  (assoc-data source (try
+                       (-> source val :path io/file slurp)
+                       (catch Exception e
+                         {}))))
 
-(defn environment []
-  {:type :environment
-   :data (System/getenv)})
+(defmethod load-handler :kyushu/resource [source]
+  (assoc-data source (try
+                       (-> source val :path io/resource slurp)
+                       (catch Exception e
+                         {}))))
 
-(defn refresh [source]
-  (into {}
-    (map (fn [[name v]]
-           (let [{:keys [type path]} v]
-             {name (case type
-                     :file (file path)
-                     :resource (resource path)
-                     v)}))
-         source)))
+(defmethod load-handler :kyushu/environment [source]
+  (assoc-data source (System/getenv)))
+
+(defmethod load-handler :default [source]
+  source)
+
+(defn refresh [provider]
+  (into {} (map load-handler provider)))
 
 (defn- merge-in
   ([a] a)
@@ -44,9 +36,9 @@
   ([a b & more]
    (reduce merge-in (merge-in a b) more)))
 
-(defn ask [source ks]
-  (get-in (->> (vals source)
-               (map :data)
-               (apply merge-in))
-          ks))
+(defn- merge-sources [provider]
+  (->> provider vals (map :data) (apply merge-in)))
+
+(defn ask [provider ks]
+  (get-in (merge-sources provider) ks))
 
